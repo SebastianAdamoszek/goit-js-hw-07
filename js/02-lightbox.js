ab7b9d3fd9e2e5979b685355342ad37257527c99
Seba <sebaadamoszek@vp.pl>
2023-09-09 13:27:52 +0200

    ready task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemsHTML = galleryItems.map(image => `
+<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>
+`).join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryItemsHTML);
+
+
+// Inicjalizacja SimpleLightbox z opcją wyświetlania podpisów
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+// Uruchom galerię
+lightbox.open();
+
+
+// console.log(galleryItems);
