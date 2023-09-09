ef0d1597aa07789287e8d53f14211127e8b834ed
Seba <sebaadamoszek@vp.pl>
2023-09-09 20:41:18 +0200

    corrected modal and key escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryList = document.querySelector(".gallery");
 
@@ -14,7 +13,6 @@ const galleryItemsHTML = galleryItems.map(image => `
     />
   </a>
 </div>
-
 `).join("");
 
 galleryList.insertAdjacentHTML("beforeend", galleryItemsHTML);
@@ -24,60 +22,41 @@ const modal = document.querySelector(".modal");
 const modalOverlay = document.querySelector(".modal__overlay");
 const modalImage = document.querySelector("#modalImage");
 
-// Kliknięcie na miniaturę
-galleryList.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  const clickedImage = event.target;
-
-  // Warunek dla sprawdzenia czy kliknięty element to obraz
-  if (clickedImage.nodeName === "IMG") {
-    // Pobieranie zawartości data-source z klikniętego obrazka
-    const originalImageUrl = clickedImage.dataset.source;
-
-    // Pobieranie źródła obrazu wewnątrz okna modalnego
-    modalImage.src = originalImageUrl;
-
-    // Otwieranie okna modalnego
-    modal.classList.add("is-open");
-  }
-});
-
-
 // Obsługa kliknięcia na overlay lub obraz wewnątrz modala, aby go zamknąć
 modalOverlay.addEventListener("click", closeModal);
-modalImage.addEventListener("click", closeModal);
 
-// Zamknięcie okna modalnego klawiszem "Escape"
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    closeModal();
-  }
-});
+function openModal(originalImageUrl) {
+  // Pobieranie źródła obrazu wewnątrz okna modalnego
+  modalImage.innerHTML = `<img src="${originalImageUrl}" width="800" height="600">`;
+
+  // Otwieranie okna modalnego
+  modal.classList.add("is-open");
+
+  // Dodajemy słuchacza klawiatury tylko, gdy modal jest otwarty
+  document.addEventListener("keydown", keyboardListener);
+}
 
 function closeModal() {
   modal.classList.remove("is-open");
-  modalImage.src = "";
+  modalImage.innerHTML = "";
+
+  // Usuwamy słuchacza klawiatury, gdy modal jest zamknięty
+  document.removeEventListener("keydown", keyboardListener);
+}
+
+function keyboardListener(event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
 }
 
 // Obsługa kliknięcia na miniaturkę
-const galleryImages = document.querySelectorAll(".gallery__image");
-
-galleryImages.forEach(image => {
-  image.addEventListener("click", () => {
-    const originalImageUrl = image.dataset.source;
-
-    // Pobieranie dużego zdjęcia z linka. basicLightbox
-    const lightbox = basicLightbox.create(`
-      <img src="${originalImageUrl}" width="800" height="600">
-    `);
-
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  const clickedImage = event.target;
+  if (clickedImage.nodeName === "IMG") {
+    const originalImageUrl = clickedImage.dataset.source;
     // Otwarcie okna modalnego
-    lightbox.show();
-  });
+    openModal(originalImageUrl);
+  }
 });
-
-
-
-
-
