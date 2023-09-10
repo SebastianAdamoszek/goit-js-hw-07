635e67c46472534fb6649d4c5966e44915a27e00
SebastianAdamoszek <sebaadamoszek@vp.pl>
2023-09-10 20:39:28 +0200

    corrected task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,16 +3,16 @@ import { galleryItems } from './gallery-items.js';
 const galleryList = document.querySelector(".gallery");
 
 const galleryItemsHTML = galleryItems.map(image => `
-<div class="gallery__item">
-  <a class="gallery__link" href="${image.original}">
-    <img
-      class="gallery__image"
-      src="${image.preview}"
-      data-source="${image.original}"
-      alt="${image.description}"
-    />
-  </a>
-</div>
+  <div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </div>
 `).join("");
 
 galleryList.insertAdjacentHTML("beforeend", galleryItemsHTML);
@@ -22,41 +22,39 @@ const modal = document.querySelector(".modal");
 const modalOverlay = document.querySelector(".modal__overlay");
 const modalImage = document.querySelector("#modalImage");
 
-// Obsługa kliknięcia na overlay lub obraz wewnątrz modala, aby go zamknąć
-modalOverlay.addEventListener("click", closeModal);
-
-function openModal(originalImageUrl) {
-  // Pobieranie źródła obrazu wewnątrz okna modalnego
-  modalImage.innerHTML = `<img src="${originalImageUrl}" width="800" height="600">`;
-
-  // Otwieranie okna modalnego
-  modal.classList.add("is-open");
-
-  // Dodajemy słuchacza klawiatury tylko, gdy modal jest otwarty
-  document.addEventListener("keydown", keyboardListener);
-}
-
-function closeModal() {
-  modal.classList.remove("is-open");
-  modalImage.innerHTML = "";
-
-  // Usuwamy słuchacza klawiatury, gdy modal jest zamknięty
-  document.removeEventListener("keydown", keyboardListener);
-}
-
-function keyboardListener(event) {
-  if (event.key === "Escape") {
-    closeModal();
-  }
-}
-
 // Obsługa kliknięcia na miniaturkę
 galleryList.addEventListener("click", (event) => {
   event.preventDefault();
+
   const clickedImage = event.target;
+
+  // Warunek dla sprawdzenia czy kliknięty element to obraz
   if (clickedImage.nodeName === "IMG") {
+    // Pobieranie zawartości data-source z klikniętego obrazka
     const originalImageUrl = clickedImage.dataset.source;
+
+    // Tworzenie instancji SimpleLightbox
+    const lightbox = basicLightbox.create(`
+      <img src="${originalImageUrl}" width="800" height="600">
+    `);
+
     // Otwarcie okna modalnego
-    openModal(originalImageUrl);
+    lightbox.show();
+
+    // Obsługa zamknięcia klawiszem "Escape"
+    const onEscape = (event) => {
+      if (event.key === "Escape") {
+        lightbox.close();
+        document.removeEventListener("keydown", onEscape);
+      }
+    };
+
+    document.addEventListener("keydown", onEscape);
   }
 });
+
+
+
+
+
+
